4f3da4aba4044ef3f3f2d51a1169b2b67ac67662
Alan Hurtarte <alan@mypostcard.com>
2021-04-16 09:05:47 -0600

    Improve responsiveness

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import './App.css';
-import { Navbar } from './components/navbar';
-import Player from './player';
+import "./App.css";
+import { Navbar } from "./components/navbar";
+import Player from "./player";
 
 function App() {
-
   let book_1 = {
-    cover: 'https://images.findawayworld.com/v1/image/cover/CD288713?width=350&height=350',
-    audio: 'https://samples.findawayworld.com/302699/302699_sample.mp3',
-    title: 'Surfear la vida',
-    author: 'Jaimal Yogis',
-    narrator: 'Narrated by Adriana Galindo',
-    description:`Jaimal Yogis deja la secundaria urbana para escapar a Hawái con un par de libros y una inquietud por aprender a surfear. En la isla, lejos de la vida urbana, emprende un viaje repleto de altibajos, que pronto se convertirá en una travesía de aprendizaje. Misma que lo llevará desde las cabañas y cálidas olas del Pacífico, hasta los barcos de bahía, universidades, monasterios y aguas heladas de Brooklyn.
+    cover:
+      "https://images.findawayworld.com/v1/image/cover/CD288713?width=350&height=350",
+    audio: "https://samples.findawayworld.com/302699/302699_sample.mp3",
+    title: "Surfear la vida",
+    author: "Jaimal Yogis",
+    narrator: "Narrated by Adriana Galindo",
+    description: `Jaimal Yogis deja la secundaria urbana para escapar a Hawái con un par de libros y una inquietud por aprender a surfear. En la isla, lejos de la vida urbana, emprende un viaje repleto de altibajos, que pronto se convertirá en una travesía de aprendizaje. Misma que lo llevará desde las cabañas y cálidas olas del Pacífico, hasta los barcos de bahía, universidades, monasterios y aguas heladas de Brooklyn.
     ¿Te has preguntado en qué se parece nuestro pensamiento al ritmo de las olas del mar?, ¿estamos en control o nos adaptamos al oleaje de la vida? Sumérgete en el viaje espiritual que supone esta obra y descubre cómo el autor construye una profunda relación entre el surf y la búsqueda del ser.`,
     meta: `Language:Spanish
 
     Runtime:04:53:24
-    
-    Published by Beek Technologies, Inc the 2019-08-23T00:00:00Z`,
 
+    Published by Beek Technologies, Inc the 2019-08-23T00:00:00Z`,
   };
 
   let book_2 = {
-    cover: 'https://images.findawayworld.com/v1/image/cover/CD396222?width=350&height=350',
-    audio: 'https://samples.findawayworld.com/411359/411359_sample.mp3',
-    title: 'El poder del ahora, en 15 minutos...',
-    author: 'Eckhart Tolle',
-    narrator: 'Narrated by Adriana Galindo',
-    description:`El libro que consagró a Eckhart Tolle como uno de los gurús más importantes del mundo: 'El poder del ahora' . Considerado hoy en día una obra maestra, un libro que deja un mensaje único: se puede alcanzar un estado de iluminación aquí y ahora, es posible vivir libre del sufrimiento, de la ansiedad y de la neurosis. Para lograrlo sólo tenemos que comprender nuestro papel de creadores de nuestro dolor. El poder del ahora es una invitación a la reflexión, para abrirle las puertas a la plenitud espiritual y poder ver la vida con nuevos ojos para empezar a disfrutar del verdadero poder del ahora. Las actividades de Beek son realizadas bajo el amparo del ejercicio del derecho a la libertad de expresión.`,
+    cover:
+      "https://images.findawayworld.com/v1/image/cover/CD396222?width=350&height=350",
+    audio: "https://samples.findawayworld.com/411359/411359_sample.mp3",
+    title: "El poder del ahora, en 15 minutos...",
+    author: "Eckhart Tolle",
+    narrator: "Narrated by Adriana Galindo",
+    description: `El libro que consagró a Eckhart Tolle como uno de los gurús más importantes del mundo: 'El poder del ahora' . Considerado hoy en día una obra maestra, un libro que deja un mensaje único: se puede alcanzar un estado de iluminación aquí y ahora, es posible vivir libre del sufrimiento, de la ansiedad y de la neurosis. Para lograrlo sólo tenemos que comprender nuestro papel de creadores de nuestro dolor. El poder del ahora es una invitación a la reflexión, para abrirle las puertas a la plenitud espiritual y poder ver la vida con nuevos ojos para empezar a disfrutar del verdadero poder del ahora. Las actividades de Beek son realizadas bajo el amparo del ejercicio del derecho a la libertad de expresión.`,
     meta: `Language:Spanish
 
     Runtime:00:07:26
-    
-    Published by Beek Technologies, Inc the 2021-02-15T00:00:00Z`,
-  }
 
-  return (
-      <Player book={book_2}></Player>
-  );
+    Published by Beek Technologies, Inc the 2021-02-15T00:00:00Z`,
+  };
+
+  return <Player book={book_2}></Player>;
 }
 
 export default App;
--- src/player/index.js
+++ src/player/index.js
@@ -1,216 +1,290 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
-import ForwardIcon from '../icons/fast_forward_black_24dp.svg';
-import RewindIcon from '../icons/fast_rewind_black_24dp.svg';
-import SpeedIcon from '@material-ui/icons/Speed';
-import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
-import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
-import Forward10Icon from '@material-ui/icons/Forward10';
-import Replay10Icon from '@material-ui/icons/Replay10';
-import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import React from "react";
+import ReactPlayer from "react-player";
+import SpeedIcon from "@material-ui/icons/Speed";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import PauseCircleFilledIcon from "@material-ui/icons/PauseCircleFilled";
+import Forward10Icon from "@material-ui/icons/Forward10";
+import Replay10Icon from "@material-ui/icons/Replay10";
+import VolumeUpIcon from "@material-ui/icons/VolumeUp";
 
 class Player extends React.Component {
-    constructor(props) {
-        super(props);
-        this.player = null;
-        this.state = { 
-            playing: false,
-            speed: 1,
-            played: 0,
-            seeking: false,
-            duration: 0,
-            volume: 0.8,
-        };
+  constructor(props) {
+    super(props);
+    this.player = null;
+    this.state = {
+      playing: false,
+      speed: 1,
+      playbackRate: 1,
+      played: 0,
+      seeking: false,
+      duration: 0,
+      volume: 0.8,
+    };
 
-        this.togglePlayState = this.togglePlayState.bind(this);
-        this.handleSetPlaybackRate = this.handleSetPlaybackRate.bind(this);
-        this.handleSeekChange = this.handleSeekChange.bind(this);
-        this.handleProgress = this.handleProgress.bind(this);
-        this.handleDuration = this.handleDuration.bind(this);
-        this.handleSeekMouseUp = this.handleSeekMouseUp.bind(this);
-        this.handleSeekMouseDown = this.handleSeekMouseDown.bind(this);
-        this.handleRewind = this.handleRewind.bind(this);
-        this.handleForward = this.handleForward.bind(this);
-        this.handleVolumeChange = this.handleVolumeChange.bind(this);
-    
-      }
+    this.togglePlayState = this.togglePlayState.bind(this);
+    this.handleSetPlaybackRate = this.handleSetPlaybackRate.bind(this);
+    this.handleSeekChange = this.handleSeekChange.bind(this);
+    this.handleProgress = this.handleProgress.bind(this);
+    this.handleDuration = this.handleDuration.bind(this);
+    this.handleSeekMouseUp = this.handleSeekMouseUp.bind(this);
+    this.handleSeekMouseDown = this.handleSeekMouseDown.bind(this);
+    this.handleRewind = this.handleRewind.bind(this);
+    this.handleForward = this.handleForward.bind(this);
+    this.handleVolumeChange = this.handleVolumeChange.bind(this);
+  }
 
-      togglePlayState() {
-        this.setState(
-            {
-                playing: !this.state.playing
-            }
-        )
-      }
+  togglePlayState() {
+    this.setState({
+      playing: !this.state.playing,
+    });
+  }
 
-      handleProgress = state => {
-        console.log('onProgress', state)
-        // We only want to update time slider if we are not currently seeking
-        if (!this.state.seeking) {
-          this.setState(state)
-        }
-      }
+  handleProgress = (state) => {
+    console.log("onProgress", state);
+    // We only want to update time slider if we are not currently seeking
+    if (!this.state.seeking) {
+      this.setState(state);
+    }
+  };
 
-      handleSetPlaybackRate = e => {
-        this.setState({ playbackRate: parseFloat(e.target.value) })
-      }
+  handleSetPlaybackRate = (e) => {
+    this.setState({ playbackRate: parseFloat(e.target.value) });
+  };
 
-      handleSeekMouseDown = e => {
-        this.setState({ seeking: true })
-      }
-    
-      handleSeekChange = e => {
-        this.setState({ played: parseFloat(e.target.value) })
-      }
-    
-      handleSeekMouseUp = e => {
-        this.setState({ seeking: false })
-        this.player.seekTo(parseFloat(e.target.value))
-      }
+  handleSeekMouseDown = (e) => {
+    this.setState({ seeking: true });
+  };
 
-      handleDuration = (duration) => {
-        console.log('onDuration', duration)
-        this.setState({ duration })
-      }
+  handleSeekChange = (e) => {
+    this.setState({ played: parseFloat(e.target.value) });
+  };
 
-      handleRewind = e => {
-        this.player.seekTo(this.player.getCurrentTime() - 10, 'seconds');
-      }
+  handleSeekMouseUp = (e) => {
+    this.setState({ seeking: false });
+    this.player.seekTo(parseFloat(e.target.value));
+  };
 
-      handleForward = e => {
-        this.player.seekTo(this.player.getCurrentTime() + 10, 'seconds');
-      }
+  handleDuration = (duration) => {
+    console.log("onDuration", duration);
+    this.setState({ duration });
+  };
 
-      secondsToMinutesAndSeconds = (seconds) => {
-          let min = Math.floor(seconds / 60);
-          let sec = (seconds - min * 60).toFixed(0);
-          return (min < 10 ? `0${min}` : min ) + ':' + (sec < 10 ? `0${sec}` : sec )
-      }
+  handleRewind = (e) => {
+    this.player.seekTo(this.player.getCurrentTime() - 10, "seconds");
+  };
 
-      handleVolumeChange = e => {
-        this.setState({ volume: parseFloat(e.target.value) })
-      }
+  handleForward = (e) => {
+    this.player.seekTo(this.player.getCurrentTime() + 10, "seconds");
+  };
 
-      ref = player => {
-        this.player = player
-      }
+  secondsToMinutesAndSeconds = (seconds) => {
+    let min = Math.floor(seconds / 60);
+    let sec = (seconds - min * 60).toFixed(0);
+    return (min < 10 ? `0${min}` : min) + ":" + (sec < 10 ? `0${sec}` : sec);
+  };
 
-    render() { return (
-            <div className="player">
-                <div className="information-wrapper w-screen"> 
-                    <div className=" h-screen w-screen flex justify-between align-middle ">
-                        <div className="cover-wrapper m-auto">
-                            <div className="">
-                                <div className="relative">
-                                    <img className="audiobook-cover" 
-                                    src={this.props.book.cover} alt=""/>
-                                    <div className="controls-over">
-                                        <button className='play' onClick={this.togglePlayState}>
-                                            { this.state.playing ? <PauseCircleFilledIcon color={'#252626'} fontSize='inherit'/> : <PlayCircleFilledIcon color={'#252626'} fontSize='inherit' />}
-                                        </button>
-                                    </div>
-                                </div>
+  handleVolumeChange = (e) => {
+    this.setState({ volume: parseFloat(e.target.value) });
+  };
 
-                                <div className="author-info">
-                                        <h3>{this.props.book.author}</h3>
-                                        <small>{this.props.book.narrator}</small>
-                                    </div>
-                            </div>
-                        </div>
-                        <div className="chapters-wrapper glassmorphism-effect p-3">
-                            <h1>{this.props.book.title}</h1>
-                            <hr/>
-                            <p className="pt-2">{this.props.book.description}</p>
-                            <hr/>
-                            <h2>Capitulos</h2>
-                            <ul>
-                                <li>
-                                    <div className="section-name">
+  ref = (player) => {
+    this.player = player;
+  };
 
-                                    Prefacio
-                                    </div>
-                                    <ul className="chapters">
-                                        <li>Nombre 1 <span>23:45</span></li>
-                                        <li>Nombre 2 <span>12:23</span></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                <div className="section-name">
-
-                                    Parte 1
-                                    </div>
-                                    <ul className="chapters">
-                                        <li>Nombre 1<span>12:23</span></li>
-                                        <li>Nombre 2<span>12:23</span></li>
-                                        <li>Nombre 3<span>12:23</span></li>
-                                        <li>Nombre 4<span>12:23</span></li>
-                                        <li>Nombre 5<span>12:23</span></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                <div className="section-name">
-
-                                    Parte 2
-                                    </div>
-                                    <ul className="chapters">
-                                        <li>Nombre 1<span>12:23</span></li>
-                                        <li>Nombre 2<span>12:23</span></li>
-                                        <li>Nombre 3<span>12:23</span></li>
-                                        <li>Nombre 4<span>12:23</span></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                            <div className="legal-info">
-                            {this.props.book.meta}
-                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="player-controls w-screen glassmorphism-effect">
-                    <div className="left-controls flex-row">        
-                        <button onClick={this.handleRewind}><Replay10Icon color={'#252626'} fontSize='default'/></button>
-                        <button className='play' onClick={this.togglePlayState}>{ this.state.playing ? <PauseCircleFilledIcon color={'#252626'} fontSize='inherit'/> : <PlayCircleFilledIcon color={'#252626'} fontSize='inherit' />}</button>
-                        <button onClick={this.handleForward}><Forward10Icon color={'#252626'} fontSize='default'/></button>
-                    </div>
-                    <div className="length-controls">
-                        <span className='current-time'>{this.secondsToMinutesAndSeconds(this.state.duration * this.state.played)}</span>
-                        <input
-                        className="seek-bar" 
-                        type='range' min={0} max={0.999999} step='any'
-                        value={this.state.played}
-                        onMouseDown={this.handleSeekMouseDown}
-                        onChange={this.handleSeekChange}
-                        onMouseUp={this.handleSeekMouseUp}
+  render() {
+    return (
+      <div className="player">
+        <div className="information-wrapper w-screen">
+          <div className=" h-screen w-screen flex justify-between align-middle">
+            <div className="cover-wrapper m-auto">
+              <div className="">
+                <div className="relative">
+                  <img
+                    className="audiobook-cover sm:w-auto"
+                    src={this.props.book.cover}
+                    alt=""
+                  />
+                  <div className="controls-over">
+                    <button className="play" onClick={this.togglePlayState}>
+                      {this.state.playing ? (
+                        <PauseCircleFilledIcon
+                          color={"#252626"}
+                          fontSize="inherit"
                         />
-                        <span className="elapsed-time">{this.secondsToMinutesAndSeconds(this.state.duration * (1 - this.state.played))}</span>
-                    </div>
-                    
-                    <div className="speed-controls flex justify-center">
-                        <SpeedIcon color={'#252626'}/>
-                        <button className="mr-2" onClick={this.handleSetPlaybackRate} value={1}>1x</button>
-                        <button className="mr-2" onClick={this.handleSetPlaybackRate} value={1.5}>1.5x</button>
-                        <button className="mr-2" onClick={this.handleSetPlaybackRate} value={2}>2x</button>
-                    </div>
-
-                    <div className="volume-controls flex justify-center">
-                        <VolumeUpIcon color={'#252626'}/>
-                        <input className='volume-bar' type='range' min={0} max={1} step='any' value={this.state.volume} onChange={this.handleVolumeChange} />
-                     </div>
-
-                    <ReactPlayer 
-                        style={{'display': 'none'}}
-                        onProgress={this.handleProgress} 
-                        playbackRate={this.state.playbackRate} 
-                        ref={this.ref}
-                        volume={this.state.volume}
-                        playing={this.state.playing} 
-                        onDuration={this.handleDuration}
-                        url={this.props.book.audio}></ReactPlayer>
+                      ) : (
+                        <PlayCircleFilledIcon
+                          color={"#252626"}
+                          fontSize="inherit"
+                        />
+                      )}
+                    </button>
+                  </div>
                 </div>
-                
-            </div>  
-        )
-    } 
+
+                <div className="author-info">
+                  <h3>{this.props.book.author}</h3>
+                  <small>{this.props.book.narrator}</small>
+                </div>
+              </div>
+            </div>
+            <div className="chapters-wrapper glassmorphism-effect p-3 md:w-80 hidden md:block">
+              <h1>{this.props.book.title}</h1>
+              <hr />
+              <p className="pt-2">{this.props.book.description}</p>
+              <hr />
+              <h2>Capitulos</h2>
+              <ul>
+                <li>
+                  <div className="section-name">Prefacio</div>
+                  <ul className="chapters">
+                    <li>
+                      Nombre 1 <span>23:45</span>
+                    </li>
+                    <li>
+                      Nombre 2 <span>12:23</span>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <div className="section-name">Parte 1</div>
+                  <ul className="chapters">
+                    <li>
+                      Nombre 1<span>12:23</span>
+                    </li>
+                    <li>
+                      Nombre 2<span>12:23</span>
+                    </li>
+                    <li>
+                      Nombre 3<span>12:23</span>
+                    </li>
+                    <li>
+                      Nombre 4<span>12:23</span>
+                    </li>
+                    <li>
+                      Nombre 5<span>12:23</span>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <div className="section-name">Parte 2</div>
+                  <ul className="chapters">
+                    <li>
+                      Nombre 1<span>12:23</span>
+                    </li>
+                    <li>
+                      Nombre 2<span>12:23</span>
+                    </li>
+                    <li>
+                      Nombre 3<span>12:23</span>
+                    </li>
+                    <li>
+                      Nombre 4<span>12:23</span>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <div className="legal-info">{this.props.book.meta}</div>
+            </div>
+          </div>
+        </div>
+        <div className="player-controls w-screen glassmorphism-effect ">
+          <div className="left-controls flex-row">
+            <button onClick={this.handleRewind} className="hidden md:block">
+              <Replay10Icon color={"#252626"} fontSize="default" />
+            </button>
+            <button className="play" onClick={this.togglePlayState}>
+              {this.state.playing ? (
+                <PauseCircleFilledIcon color={"#252626"} fontSize="inherit" />
+              ) : (
+                <PlayCircleFilledIcon color={"#252626"} fontSize="inherit" />
+              )}
+            </button>
+            <button onClick={this.handleForward} className="hidden md:block">
+              <Forward10Icon color={"#252626"} fontSize="default" />
+            </button>
+          </div>
+          <div className="length-controls">
+            <span className="current-time">
+              {this.secondsToMinutesAndSeconds(
+                this.state.duration * this.state.played
+              )}
+            </span>
+            <input
+              className="seek-bar"
+              type="range"
+              min={0}
+              max={0.999999}
+              step="any"
+              value={this.state.played}
+              onMouseDown={this.handleSeekMouseDown}
+              onChange={this.handleSeekChange}
+              onMouseUp={this.handleSeekMouseUp}
+            />
+            <span className="elapsed-time">
+              {this.secondsToMinutesAndSeconds(
+                this.state.duration * (1 - this.state.played)
+              )}
+            </span>
+          </div>
+
+          <div className="speed-controls flex justify-center hidden md:flex">
+            <SpeedIcon color={"#252626"} />
+            <button
+              className={
+                this.state.playbackRate === 1 ? "mx-2 font-bold" : "mx-2"
+              }
+              onClick={this.handleSetPlaybackRate}
+              value={1}
+            >
+              1x
+            </button>
+            <button
+              className={
+                this.state.playbackRate === 1.5 ? "mx-2 font-bold" : "mx-2"
+              }
+              onClick={this.handleSetPlaybackRate}
+              value={1.5}
+            >
+              1.5x
+            </button>
+            <button
+              className={
+                this.state.playbackRate === 2 ? "mx-2 font-bold" : "mx-2"
+              }
+              onClick={this.handleSetPlaybackRate}
+              value={2}
+            >
+              2x
+            </button>
+          </div>
+
+          <div className="volume-controls flex justify-center">
+            <VolumeUpIcon color={"#252626"} />
+            <input
+              className="volume-bar"
+              type="range"
+              min={0}
+              max={1}
+              step="any"
+              value={this.state.volume}
+              onChange={this.handleVolumeChange}
+            />
+          </div>
+
+          <ReactPlayer
+            style={{ display: "none" }}
+            onProgress={this.handleProgress}
+            playbackRate={this.state.playbackRate}
+            ref={this.ref}
+            volume={this.state.volume}
+            playing={this.state.playing}
+            onDuration={this.handleDuration}
+            url={this.props.book.audio}
+          ></ReactPlayer>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Player;
